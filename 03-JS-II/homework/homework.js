7050a897bfe9b938c5690228ee3aff99d82847d8
Braian Gomez <braiandenisgomez@gmail.com>
2022-01-11 22:21:52 -0300

    Finished homework js2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -201,7 +201,13 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  
+  let times = 0;
+  do {
+    numero += 5;
+    times++;
+  }
+  while (times < 8);
+  return numero;
 }
 
 
